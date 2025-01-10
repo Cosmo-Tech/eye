d2b7063f8c03eb35845ae5a47228567bfe2efb89
Diamantis Sellis <diamantis.sellis@cosmotech.com>
2025-01-10 12:24:13 +0100

    feat: get id and name lists

--- eye/main.py
+++ eye/main.py
@@ -41,6 +41,18 @@ class RUON:
     except Exception as e:
       print(f"error {e}")
 
+  def get_organization_list(self):
+    return [organization.id for organization in self.organizations]
+
+  def get_solution_list(self, organization_id):
+    return [(solution.id, solution.name) for solution in self.solutions.get(organization_id, [])]
+
+  def get_workspace_list(self, organization_id):
+    return [workspace.id for workspace in self.workspaces.get(organization_id, [])]
+
+  def get_runner_list(self, organization_id, workspace_id):
+    return [(runner.id, runner.name) for runner in self.runners.get((organization_id, workspace_id), [])]
+
   def update_solutions(self, organization_id):
     try:
       self.solutions[organization_id] = self.solution_api_instance.find_all_solutions(organization_id)
