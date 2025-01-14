1fe31567d998f46d271dd14ade29078174360e24
Diamantis Sellis <diamantis.sellis@cosmotech.com>
2025-01-14 10:05:45 +0100

    fix: organize users view

--- eye/app.py
+++ eye/app.py
@@ -1,21 +1,35 @@
 from textual.app import App, ComposeResult
-from textual.widgets import DataTable, Pretty, Tree, Footer, Header, ListView, ListItem, Label, Static
-from textual.containers import Horizontal, Container
+from textual.widgets import (
+    DataTable,
+    Pretty,
+    Tree,
+    Footer,
+    Header,
+    ListView,
+    ListItem,
+    Label,
+    Static,
+)
+from textual.containers import Horizontal, Container, Vertical
 from textual.widget import Widget
 from textual.reactive import reactive
 from textual import on
 from eye.main import RUON
 from pathlib import Path
+import pandas as pd
 
 
 class Solution(Widget):
-    def __init__(self, manager, organization,**kwargs):
+    def __init__(self, manager, organization, **kwargs):
         super().__init__(**kwargs)
         self.manager = manager
         self.organization = organization
 
     def compose(self) -> ComposeResult:
-        solutions = [ListItem(Label(item)) for item in self.manager.get_solution_list(self.organization)]
+        solutions = [
+            ListItem(Label(item))
+            for item in self.manager.get_solution_list(self.organization)
+        ]
         self.solution_view = ListView(*solutions)
         yield self.solution_view
 
@@ -26,14 +40,18 @@ class Solution(Widget):
         for item in solutions:
             self.solution_view.append(ListItem(Label(item)))
 
+
 class Workspace(Widget):
-    def __init__(self, manager, organization,**kwargs):
+    def __init__(self, manager, organization, **kwargs):
         super().__init__(**kwargs)
         self.manager = manager
         self.organization = organization
 
     def compose(self) -> ComposeResult:
-        workspaces = [ListItem(Label(item)) for item in self.manager.get_workspace_list(self.organization)]
+        workspaces = [
+            ListItem(Label(item))
+            for item in self.manager.get_workspace_list(self.organization)
+        ]
         self.workspace_view = ListView(*workspaces)
         yield self.workspace_view
 
@@ -44,40 +62,42 @@ class Workspace(Widget):
         for item in workspaces:
             self.workspace_view.append(ListItem(Label(item)))
 
-class Security(Container):
 
+class Security(Container):
     def __init__(self, manager, organization, **kwargs):
         super().__init__(**kwargs)
         self.manager = manager
         self.organization = organization
 
     def compose(self) -> ComposeResult:
-      table = DataTable(id = "security")
-      table.border_title = "Security"
-      df = self.manager.get_security_dataframe(self.organization)
-      table.add_columns("User", *df.columns.tolist())
-      for idx, row in df.iterrows():
-        table.add_row(idx, *row.tolist())
-      yield table
+        table = DataTable(id="security")
+        table.border_title = "Security"
+        df = self.manager.get_security_dataframe(self.organization)
+        table.add_columns("User", *df.columns.tolist())
+        for idx, row in df.iterrows():
+            table.add_row(idx, *row.tolist())
+        yield table
 
     def update_security(self, organization) -> None:
         self.organization = organization
         table = self.query_one("#security", DataTable)
         df = self.manager.get_security_dataframe(self.organization)
-        table.clear()
+        table.clear(columns=True)
         table.add_columns("User", *df.columns.tolist())
+
         for idx, row in df.iterrows():
             table.add_row(idx, *row.tolist())
+
 
 class TUI(App):
     BINDINGS = [
         ("h", "help", "Help"),
         ("q", "quit", "Quit"),
-        ("s", "switch_view", "Switch View")
+        ("s", "switch_view", "Switch View"),
     ]
     CSS_PATH = Path(__file__).parent / "styles.tcss"
     active_organization = reactive("")
-    
+
     def __init__(self, manager) -> None:
         super().__init__()
         self.organization_view = ListView()
@@ -88,56 +108,70 @@ class TUI(App):
         """Create children for layout"""
         yield Header()
         yield Footer()
-        
-        organizations = [ListItem(Label(item)) for item in self.manager.get_organization_list()]
+
+        organizations = [
+            ListItem(Label(item)) for item in self.manager.get_organization_list()
+        ]
         self.organization_view = ListView(*organizations, id="organizations_view")
-        self.organization_view.border_title="Organizations"
-        self.workspace_view = Workspace(self.manager, self.active_organization, id="workspaces")
-        self.solution_view = Solution(self.manager, self.active_organization, id="solutions")
+        self.organization_view.border_title = "Organizations"
+        self.workspace_view = Workspace(
+            self.manager, self.active_organization, id="workspaces"
+        )
+        self.solution_view = Solution(
+            self.manager, self.active_organization, id="solutions"
+        )
         self.tree_view = self.build_tree()
-        self.pretty_view = Pretty({}, id = "pretty")
-        self.security_view = Security(self.manager, self.active_organization, id="security_view")
+        self.pretty_view = Pretty({}, id="pretty")
+        self.security_view = Security(
+            self.manager, self.active_organization, id="security_view"
+        )
         # Main view components
         self.solution_view = Static("adeio1")
-        #self.workspace_view = Static("adeio2")
+        # self.workspace_view = Static("adeio2")
         self.main_container = Container(
             Horizontal(
                 Container(self.tree_view),
                 Container(
                     self.organization_view,
-                    self.security_view,
-                    #self.organization_view,
-                    #self.pretty_view,
-                    #self.workspace_view,
-                    self.solution_view
-                                    )
+                    #                    self.security_view,
+                    # self.organization_view,
+                    self.pretty_view,
+                    # self.workspace_view,
+                    #                    self.solution_view
+                ),
             ),
-            id="main_view"
+            id="main_view",
         )
-        
+
         # Alternative view
-        self.alt_container = Static("adeio")
-        self.alt_container.display = False
+        # Security view
+        self.users_container = Container(
+            Vertical(self.organization_view, self.security_view)
+        )
+        self.users_container.display = False
         yield self.main_container
-        yield self.alt_container
+        yield self.users_container
 
     def build_tree(self):
-      tree = Tree("Objects", id="object_view")
-      tree.root.expand()
-      for organization in self.manager.organizations:
-        org_node = tree.root.add(organization.id, expand=True)
-        for workspace in self.manager.workspaces.get(organization.id, []):
-          workspace_node = org_node.add(workspace.id)
-          workspace_node.data = {"organization":organization.id}
-          for runner in self.manager.runners.get((organization.id, workspace.id), []):
-            runner_node = workspace_node.add(runner.id)
-            runner_node.data = {"organization":organization.id,
-                                "workspace":workspace.id}
-        for solution in self.manager.solutions.get(organization.id, []):
-          solution_node = org_node.add(solution.id)
-          solution_node.data = {"organization":organization.id}
-      return tree
-
+        tree = Tree("Objects", id="object_view")
+        tree.root.expand()
+        for organization in self.manager.organizations:
+            org_node = tree.root.add(organization.id, expand=True)
+            for workspace in self.manager.workspaces.get(organization.id, []):
+                workspace_node = org_node.add(workspace.id)
+                workspace_node.data = {"organization": organization.id}
+                for runner in self.manager.runners.get(
+                    (organization.id, workspace.id), []
+                ):
+                    runner_node = workspace_node.add(runner.id)
+                    runner_node.data = {
+                        "organization": organization.id,
+                        "workspace": workspace.id,
+                    }
+            for solution in self.manager.solutions.get(organization.id, []):
+                solution_node = org_node.add(solution.id)
+                solution_node.data = {"organization": organization.id}
+        return tree
 
     @on(ListView.Highlighted, "#organizations_view")
     def organization_changed(self, message: ListView.Highlighted) -> None:
@@ -158,16 +192,17 @@ class TUI(App):
         """Toggle between main and alternative views"""
         self.is_main_view = not self.is_main_view
         self.main_container.display = self.is_main_view
-        self.alt_container.display = not self.is_main_view
+        self.users_container.display = not self.is_main_view
+
 
 if __name__ == "__main__":
-    host = 'http://localhost:8080'
+    host = "http://localhost:8080"
     manager = RUON(host=host)
     manager.update_organizations()
     for organization in manager.organizations:
-      manager.update_workspaces(organization.id)
-      manager.update_solutions(organization.id)
-      for workspace in manager.workspaces[organization.id]:
-        manager.update_runners(organization.id, workspace.id)
+        manager.update_workspaces(organization.id)
+        manager.update_solutions(organization.id)
+        for workspace in manager.workspaces[organization.id]:
+            manager.update_runners(organization.id, workspace.id)
     app = TUI(manager)
     app.run()
